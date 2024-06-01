69d5154a335c761590ba60febee5b6a2349cf5d4
molejnik <molejnik@pop-os.home>
2024-06-01 21:13:46 +0200

    more hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["Dog", "Cat", "Bird"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +19,36 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            value={animal}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            onChange={(e) => setAnimal(e.target.value)}
+            value={breed}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
